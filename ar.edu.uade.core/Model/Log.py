5ff1344a6322a797596f6735a62aba2d6611ff4a
Maxmart27 <Maximmart007@gmail.com>
2024-09-27 17:53:43 -0300

    Completado retries, continuacion de Logs

---
+++ ar.edu.uade.core/Model/Log.py
@@ -0,0 +1,20 @@
+import uuid
+from cassandra.cqlengine import columns
+from cassandra.cqlengine.models import Model
+
+from Database.Cassandra import syncronize_cassandra_tables
+
+
+class LogModel(Model):
+    id = columns.UUID(primary_key=True, default=uuid.uuid4)
+    status = columns.Integer(index=True)
+    timestamp = columns.DateTime(index=True, clustering_order='DESC')
+    ttl = columns.Integer()
+    payload = columns.Text()
+    origin = columns.Text(index=True)
+    destination = columns.Text(index=True)
+
+def save_log(status, timestamp, ttl, payload, origin, destination):
+    LogModel.create(status=status, timestamp=timestamp, ttl=ttl, payload=payload, origin=origin, destination=destination)
+    syncronize_cassandra_tables()
+
--- ar.edu.uade.core/Senders/Usuario.py
+++ ar.edu.uade.core/Senders/Usuario.py
@@ -1,17 +1,13 @@
-import pika
 from pika.exceptions import UnroutableError, ConsumerCancelled
 import json as js
-from app import connection
 
-def inicializar_usuario():
-    channel = connection.channel()
+def inicializar_usuario(channel):
 
     channel.confirm_delivery()
 
     channel.exchange_declare(exchange='usuario', exchange_type='direct',durable=True)
 
     channel.queue_declare(queue='usuario', exclusive=False,durable=True,arguments={
-        'x-message-ttl':60000,
         'x-dead-letter-exchange': 'usuario',  # Enviar mensajes fallidos aquí
         'x-dead-letter-routing-key': 'usuario.retry'     # Routing key para los mensajes fallidos
     })
@@ -33,15 +29,14 @@ def inicializar_usuario():
     channel.queue_bind(
         exchange='usuario', queue='usuario.dead-letter', routing_key='usuario.dead-letter')
 
-def callback(method, properties, body):
-    #EL CANAL
-    channel = connection.channel()
+def callback(channel,method, properties, body):
+
     # Decodificar el mensaje como JSON
     json_msg = js.loads(body.decode())
     print(" [x] Received %r" % json_msg)
     # ttl del json es el retry
     ttl = int(json_msg.get("ttl", 0))
-    if 0 < ttl <= 3:
+    if 0 < ttl <= 3: #TODO poner limite personalizable
         # Reducir el TTL
         json_msg["ttl"] = ttl - 1
 
@@ -51,15 +46,14 @@ def callback(method, properties, body):
         print(f" [x] Reenviado a 'usuario' con TTL actualizado: {nuevo_mensaje}")
     else:
         nuevo_mensaje = js.dumps(json_msg)
-        channel.basic_publish(exchange='usuario', routing_key='usuario.deadletter', body=nuevo_mensaje, mandatory=True)
+        channel.basic_publish(exchange='usuario', routing_key='usuario.dead-letter', body=nuevo_mensaje, mandatory=True)
     # Acknowledge el mensaje para eliminarlo de la cola de retry
     channel.basic_ack(delivery_tag=method.delivery_tag)
 
 
     # Definir la función que se ejecutará cuando se reciba un mensaje
 
-def publish_usuario(message):
-    channel = connection.channel()
+def publish_usuario(channel,message):
 #Decirle a RabbitMQ que queremos recibir mensajes de 'Usuario'
     try:
         channel.basic_publish(exchange='usuario', routing_key='usuario', body=message,mandatory=True)
@@ -67,11 +61,11 @@ def publish_usuario(message):
     except UnroutableError:
         print("El mensaje no pudo ser confirmado o enrutado desde Usuario")
 
-def consume_usuario():
-    channel = connection.channel()
+def consume_usuario(channel):
     try:
         channel.basic_consume(queue='usuario.retry', on_message_callback=callback, auto_ack=False)
         channel.start_consuming()
     except ConsumerCancelled:
         pass
+
 
