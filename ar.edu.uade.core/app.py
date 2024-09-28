8cabf8dde51b4a04398278c9b93424a46ecbe9d1
matiasfelau <matiasfelau@gmail.com>
2024-09-27 21:46:25 -0300

    agregadas variables de entorno FLASK_HOST, FLASK_PORT y SECRET_KEY

--- ar.edu.uade.core/app.py
+++ ar.edu.uade.core/app.py
@@ -1,4 +1,5 @@
 import json
+import os
 import threading
 from flask_socketio import SocketIO, emit
 from flask import Flask, render_template
@@ -10,7 +11,9 @@ from Database.Cassandra import start_cassandra_connection
 from Senders.Usuario import inicializar_usuario, publish_usuario, consume_usuario
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key' #TODO abstraer
+host = os.getenv('FLASK_HOST', '127.0.0.1')
+port = int(os.getenv('FLASK_PORT', 5000))
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 socketio = SocketIO(app)
 
@@ -52,13 +55,15 @@ def handle_message(data):
     print('Received message:', data)
     socketio.emit('response', 'Server received your message: ' + data)
 
+
+
 if __name__ == '__main__':
     t1 = threading.Thread(target=consume_usuario(channel), name='t1')
     #t2 = threading.Thread(target=consume_e_commerce(channel), name='t2')
     #t3 = threading.Thread(target=consume_gestion_financiera(channel), name='t3')
     #t4 = threading.Thread(target=consume_gestion_interna(channel), name='t4')
     #t5 = threading.Thread(target=consume_tracing(channel), name='t5')
-    t6 = threading.Thread(target=socketio.run(app, debug=True), name='t6')
+    t6 = threading.Thread(target=socketio.run(app, host=host, port=port, debug=True), name='t6')
 
     t1.start()
     #t2.start()
