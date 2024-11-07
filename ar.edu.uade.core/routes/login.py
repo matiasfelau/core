7dba8b483d1e1330d0ee36db11a32b573c8ef5d5
matiasfelau <matiasfelau@gmail.com>
2024-11-07 20:41:07 -0300

    Recibiendo cambios en el front

--- ar.edu.uade.core/routes/login.py
+++ ar.edu.uade.core/routes/login.py
@@ -9,9 +9,15 @@ from utilities import storage
 login = Blueprint('login', __name__)
 
 
-@login.route('/authlogin', methods=['POST'])
+@login.route('/authlogin', methods=['GET'])
 def signin():
-    credentials = json.dumps(request.json)
+    username = request.args.get('username')
+    password = request.args.get('password')
+    cheison = {
+        'username': username,
+        'password': password
+    }
+    credentials = json.dumps(cheison)
     connection, channel = start_rabbitmq_connection(
         storage.rabbitmq_user_username,
         storage.rabbitmq_user_password,
